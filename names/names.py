ac330b79957c70a30d587c19f57661d632850714
Ben <cbtsao47@gmail.com>
2019-07-26 11:28:40 -0500

    complete names

--- names/names.py
+++ names/names.py
@@ -9,14 +9,17 @@ f.close()
 f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
-
+dictionary = {}
 duplicates = []
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
-
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+for name in names_1:
+    dictionary[name] = 1
+for name in names_2:
+    if name in dictionary:
+        duplicates.append(name)
 end_time = time.time()
-print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print (f"runtime: {end_time - start_time} seconds")
-
+print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print(f"runtime: {end_time - start_time} seconds")
